be0f57a821014ea5f5c59f0da22d9af7d5c42a5f
robertbaik <robertbbaik@gmail.com>
2020-06-21 20:37:47 +0900

    no message

---
+++ git-master/Assets/02.Scripts/UnityCs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UnityCs : MonoBehaviour
+{
+    public int[] arr1;
+    public int[] arr2;
+
+    private void Start()
+    {
+        arr1 = new int[5];
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            arr1[i] = i;
+        }
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            Debug.Log($"arr1.i = {arr1[i]}, arr2.i = {arr2[i]}");
+        }
+    }
+
+
+}
